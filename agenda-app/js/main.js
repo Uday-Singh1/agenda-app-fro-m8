4a75924a88fb1b817380e0594dadecb3d4dea36d
Uday-Singh G <91188742+Uday-Singh1@users.noreply.github.com>
2023-05-18 17:50:17 +0200

    finished-app-fixed-defective-previous-button

---
+++ agenda-app/js/main.js
@@ -0,0 +1,11 @@
+const UdayZijnAgenda = new AgendaApp();
+
+
+//Hier gebeur er wel wat aangezien de agenda app wordt aangemaakt
+
+
+
+
+
+//Potentiele vraag voor in de les over een zelf doen taak:
+// this.htmlElement.innerText = this.nameOfMonth;       //waarom niet :  this.htmlElement.innerText = nameOfMonth; ?
